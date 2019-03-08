d586d2df3ba57273b1bc7def1654725b16ed75c8
Christopher Harvey <chharvey@icloud.com>
2019-03-08 17:45:35 -0500

    deleteContact()

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ class UI {
   }
 
   static addContactToList(contact) {
-    const list = document.querySelector("#list-app-table");
+    const list = document.querySelector("#contact-list");
 
     const row = document.createElement("tr");
 
@@ -38,6 +38,40 @@ class UI {
       `;
     list.appendChild(row);
   }
+
+  static deleteContact(target) {
+    if (target.classList.contains("delete")) {
+      target.parentElement.parentElement.remove();
+    }
+  }
+
+  // Clear fields after submit
+  static clearFields() {
+    document.querySelector("#name").value = "";
+    document.querySelector("#company").value = "";
+    document.querySelector("#phone").value = "";
+  }
 }
 
+// Display contacts
 document.addEventListener("DOMContentLoaded", UI.displayContacts);
+// Add a contact
+document.querySelector("#contact-form").addEventListener("submit", e => {
+  // Prevent submit action
+  e.preventDefault();
+  // Get form values
+  const name = document.querySelector("#name").value;
+  const company = document.querySelector("#company").value;
+  const phone = document.querySelector("#phone").value;
+  //Instantiate a contact
+  const contact = new Contact(name, company, phone);
+  // Add contact to UI
+  UI.addContactToList(contact);
+  // Clear fields
+  UI.clearFields();
+});
+
+// Delete contact event listener
+document.querySelector("#contact-list").addEventListener("click", e => {
+  UI.deleteContact(e.target);
+});
